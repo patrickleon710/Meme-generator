fefff1685656cdfee1f4617833e228c1b7aaf875
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-27 13:34:10 +0300

    creating folder creating the header

---
+++ Meme-generator/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div>
+            <img src="../images/troll-face.png" />
+            <h2>Meme Generator</h2>
+            <h4>React Course - Project 3</h4>
+        </div>
+    )
+}
