4d7eb2be45e7be678e1ab3a290e3f3af1ddf1e09
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-27 13:57:41 +0300

    styling the header

--- Meme-generator/components/Header.jsx
+++ Meme-generator/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
-        <div>
-            <img src="../images/troll-face.png" />
-            <h2>Meme Generator</h2>
-            <h4>React Course - Project 3</h4>
+        <div className="header">
+            <img className="header--image" src="../images/troll-face.png" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--text">React Course - Project 3</h4>
         </div>
     )
 }
