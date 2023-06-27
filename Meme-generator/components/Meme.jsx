7867fed9ef166eab7a8322e835103dee9c6c3cc6
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-27 14:05:51 +0300

    creating the form with the button

---
+++ Meme-generator/components/Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme() {
+    return (
+        <main>
+            <form action="">
+                <input type="text" />
+                <input type="text" />
+                <button>Get a new meme image</button>
+            </form>
+        </main>            
+    )
+}
--- Meme-generator/src/App.jsx
+++ Meme-generator/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from '../components/Header'
+import Meme from '../components/Meme'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (    
     <div>
       <Header />
+      <Meme />
     </div>
   )
 }
